dbf73284b8b2c46294edc29d650ce492647e9eed
Sebastien Talbot <moi@sebastientalbot.com>
2017-07-25 10:24:52 -0400

    Change folder's structure

--- scripts/shared/actions/polybar/poly_music.sh
+++ scripts/shared/actions/polybar/poly_music.sh
@@ -2,9 +2,9 @@
 
 icon=""
 if pgrep -x "cmus" > /dev/null;then
-	player_status=$(~/scripts/playerctl-cmus status)
+	player_status=$(~/scripts/shared/actions/polybar/playerctl-cmus status)
 	if [[ $? -eq 0 ]]; then
-		metadata="$(~/scripts/playerctl-cmus metadata artist) - $(~/scripts/playerctl-cmus metadata title)"
+		metadata="$(~/scripts/shared/actions/polybar/playerctl-cmus metadata artist) - $(~/scripts/shared/actions/polybar/playerctl-cmus metadata title)"
 	fi
 
 	if [[ $player_status = "playing" ]]; then
