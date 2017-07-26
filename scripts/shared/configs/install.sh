86f43a0011845bb7ddcfbee6093c203bd8ce63eb
Sebastien Talbot <moi@sebastientalbot.com>
2017-07-26 14:56:20 -0400

    cmus neon theme config

--- scripts/shared/configs/install.sh
+++ scripts/shared/configs/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# vis/
 mkdir -p ~/.config/vis
 ln -f ~/scripts/shared/configs/vis/config ~/.config/vis/config
-
 mkdir -p ~/.config/vis/colors
 ln -f ~/scripts/shared/configs/vis/colors/neon ~/.config/vis/colors/neon
+
+# cmus/
+mkdir -p ~/.config/cmus
+ln -f ~/scripts/shared/configs/cmus/neon.theme ~/.config/cmus/neon.theme
